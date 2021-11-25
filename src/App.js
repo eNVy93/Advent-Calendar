5af4e8692d3f51f6346afb083ebe099e6908a002
eNVy93 <netiksis.vytautas@gmail.com>
2021-11-25 22:03:02 +0200

    Added snowflakes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import CalendarCardList from './Components/CalendarCardList';
-
-
+import SnowStorm from 'react-snowstorm';
 
 function App() {
   return (
     <div className="App">
+      <SnowStorm flakesMax = {256} followMouse = {false} flakesMaxActive = {128} animationInterval = {24}/>
       <header className="App-header">
         <h2> Merry Christmas!</h2>
         <CalendarCardList />
--- src/Components/CalendarCardList.js
+++ src/Components/CalendarCardList.js
@@ -1,8 +1,8 @@
 import CalendarCard from './CalendarCard';
 import { helper } from '../Helpers/helper';
-import Kitty from "../img/kitty.png"
 
 var christmasCards;
+
 function shuffleArray(array) {
   for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -10,7 +10,6 @@ function shuffleArray(array) {
       array[i] = array[j];
       array[j] = temp;
   }
-
   return array;
 }
 
@@ -30,12 +29,9 @@ function getCalendarDataFromLocalStorage(){
 
 
 export default function CalendarCardList() {
-  // const {movie} = props;
-  // const [open, setOpen] = useState(false);
 
   preLoadCalendarDataToLocalStorage();
   getCalendarDataFromLocalStorage();
-
   return (
     <div className="card-list">
       {christmasCards.map(card => (<CalendarCard card = {card} key = {card.id}/>))}
